cb8fa6a6bea58092192b820dbc87ea66a617bf89
LieutenantKang <blizzard1107@gmail.com>
2019-10-12 13:27:10 +0900

    Clean up Adapter Code

--- app/src/main/java/com/example/kotlinstudy/Adapter/MemoAdapter.kt
+++ app/src/main/java/com/example/kotlinstudy/Adapter/MemoAdapter.kt
@@ -1,26 +1,34 @@
 package com.example.kotlinstudy.Adapter
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinstudy.Contract.AdapterContract
+import com.example.kotlinstudy.R
 import com.example.kotlinstudy.Room.Memo
+import kotlinx.android.synthetic.main.item_memo.view.*
 
-class MemoAdapter(val context: Context) : AdapterContract.View, AdapterContract.Model, RecyclerView.Adapter<MemoViewHolder>() {
+class MemoAdapter(val context: Context) : AdapterContract.View, AdapterContract.Model, RecyclerView.Adapter<MemoAdapter.MemoViewHolder>() {
 
     private lateinit var memoList : ArrayList<Memo>
 
-    var onItemClickListener : OnItemClickListener? = null
-
     override fun onBindViewHolder(holder: MemoViewHolder, position: Int) {
-        memoList[position].let{
-            holder.onBind(it, position)
+        memoList[position].let{ item ->
+            with(holder) {
+                tvTitle.text = item.title
+            }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoViewHolder =
-            MemoViewHolder(context, parent, onItemClickListener)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_memo, parent, false)
+        return MemoViewHolder(view)
+    }
+
 
     override fun getItemCount() = memoList.size
 
@@ -34,5 +42,9 @@ class MemoAdapter(val context: Context) : AdapterContract.View, AdapterContract.
 
     override fun clearItem() {
         memoList.clear()
+    }
+
+    inner class MemoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvTitle: TextView = view.item_memo_title
     }
 }
